c33bb150a59db9b538afa7b7b1fcf9970fd4677e
Pratik-Wagley <pratikwagleywork@gmail.com>
2024-05-17 13:29:28 -0400

    Finished creating the save contract function for instance of lease contract

--- src/main/java/pluralsight/ContractDataManager.java
+++ src/main/java/pluralsight/ContractDataManager.java
@@ -8,7 +8,7 @@ public class ContractDataManager {
 
     public void saveContract(Contract contract, Dealership dealership, int vin) {
         try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter("contracts.csv"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("contracts.csv", true));
             if (contract instanceof SalesContract) {
                 SalesContract salesContract = (SalesContract) contract;
                 for (Vehicle vehicle : dealership.getAllVehicles()) {
@@ -19,13 +19,28 @@ public class ContractDataManager {
                                 + "|" + vehicle.getPrice() + "|" + salesContract.getSalesTax() + "|" + salesContract.getRecordingFee() + "|" + salesContract.getProcessingFee() + "|"
                                 + salesContract.getTotalPrice() + "|" + salesContract.isFinance() + "|" + salesContract.getMonthlyPrice());
                     }
+                    dealership.removeVehicle(vehicle);
                 }
 
+            } else if (contract instanceof LeaseContract) {
+                LeaseContract leaseContract = (LeaseContract) contract;
+                for (Vehicle vehicle : dealership.getAllVehicles()) {
+                    if (vehicle.getVin() == vin) {
+                        bw.write("LEASE" + "|" + leaseContract.getDate() + "|" + leaseContract.getName()
+                                + "|" + leaseContract.getEmail() + "|" + vehicle.getVin() + "|" + vehicle.getYear() + "|" + vehicle.getMake() + "|" + vehicle.getModel()
+                                + "|" + vehicle.getVehicleType() + "|" + vehicle.getColor() + "|" + vehicle.getOdometer()
+                                + "|" + vehicle.getPrice() + "|" + leaseContract.getExpectedEndingValue() + "|" + leaseContract.getLeaseFee() + "|" + leaseContract.getTotalPrice() + "|"
+                                + leaseContract.getMonthlyPayment());
+                    }
+                    dealership.removeVehicle(vehicle);
+
+                }
             }
+            bw.newLine();
+            bw.close();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 }
